cce1d85cfd3192683a8a52c4d5becf12669f215e
Bhagyesh-Vyas-009 <bhagyeshvyas009@gmail.com>
2024-09-17 20:16:48 +0530

    UserLoginModel added

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -30,4 +30,13 @@ namespace Coffee_Shop.Models
 
         public bool? isActive {  get; set; }
     }
+
+    public class UserLoginModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
 }
--- obj/Debug/net8.0/Coffee_Shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Coffee_Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5478d7d425975b8411bddbfc82f8eea267d039b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a5abc1b08e25d706ab68f2bf9c33f604428fbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
