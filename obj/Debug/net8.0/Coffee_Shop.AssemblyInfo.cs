05b01875dcb65c584218dbb4259b7772187f223a
Bhagyesh-Vyas-009 <bhagyeshvyas009@gmail.com>
2024-09-18 20:07:02 +0530

    Updated

--- obj/Debug/net8.0/Coffee_Shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Coffee_Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abae83333e62937eabb18663345300bf12c2fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81595fd69614d32371e9834001b8770524aaa5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coffee_Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
